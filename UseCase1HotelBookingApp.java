9cae2df31f0dc38a4f6fdc6a7900bb72f4bc5073
sharanmedha20 <sharanmedha20@gmail.com>
2026-03-23 14:06:05 +0530

    Create UseCase1HotelBookingApp.java

---
+++ UseCase1HotelBookingApp.java
@@ -0,0 +1,23 @@
+ @author YourName
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp {
+
+    /**
+     * The main method is the entry point of the Java application.
+     *
+     * @param args command-line arguments (not used)
+     */
+    public static void main(String[] args) {
+        // Print welcome message
+        System.out.println("=========================================");
+        System.out.println("Welcome to Book My Stay - Hotel Booking System");
+        System.out.println("Version: 1.0");
+        System.out.println("=========================================");
+
+        // Additional message (optional)
+        System.out.println("We are excited to help you book your perfect stay!");
+
+        // Program terminates naturally after main method finishes
+    }
+}
